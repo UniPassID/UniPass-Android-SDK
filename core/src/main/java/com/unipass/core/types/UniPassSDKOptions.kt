bcb8dc15c916a8128a8c21de01917bca117b113e
Ethan <yangyusenhit@gmail.com>
2023-01-06 18:21:53 +0800

    feat:adapt multi activities

--- core/src/main/java/com/unipass/core/types/UniPassSDKOptions.kt
+++ core/src/main/java/com/unipass/core/types/UniPassSDKOptions.kt
@@ -2,11 +2,13 @@ package com.unipass.core.types
 
 import android.content.Context
 import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 
 data class UniPassSDKOptions (
     var context: Context,
+    var activity: AppCompatActivity,
     var env: Environment,
     @Transient var redirectUrl: Uri? = null,
     var walletUrl: String? = null,
     var appSettings: AppSettings? = null
-    )
+)
