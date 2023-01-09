43cb9603b59b38fb9471e7777acf22aa9042de67
Ethan <yangyusenhit@gmail.com>
2023-01-09 21:45:03 +0800

    fix:user close browser directly

--- core/src/main/java/com/unipass/core/UniPassActivity.kt
+++ core/src/main/java/com/unipass/core/UniPassActivity.kt
@@ -1,6 +1,8 @@
 package com.unipass.core
 
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class UniPassActivity : AppCompatActivity() {
@@ -8,5 +10,13 @@ class UniPassActivity : AppCompatActivity() {
         super.onNewIntent(intent)
         setResult(RESULT_OK, intent);
         finish()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Handler(Looper.getMainLooper()).postDelayed({
+            // in case user close browser directly
+            finish()
+        }, 300)
     }
 }
