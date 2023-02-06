2ea8d66828d482434aa9150555ce84448595abb2
Ethan <yangyusenhit@gmail.com>
2023-02-06 16:54:37 +0800

    update: userInfo with authorize

--- core/src/main/java/com/unipass/core/types/UserInfo.kt
+++ core/src/main/java/com/unipass/core/types/UserInfo.kt
@@ -4,5 +4,7 @@ package com.unipass.core.types
 data class UserInfo (
     var email: String?,
     var address: String,
-    var newborn: Boolean = false
+    var newborn: Boolean = false,
+    val message: String?,
+    val signature: String?
 )
