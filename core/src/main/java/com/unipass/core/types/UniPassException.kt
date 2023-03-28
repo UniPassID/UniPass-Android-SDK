b6952d48713bf24c1ab28c529913b2423ca17151
Ethan <yangyusenhit@gmail.com>
2023-03-28 20:48:58 +0800

    format: errorMessage

--- core/src/main/java/com/unipass/core/types/UniPassException.kt
+++ core/src/main/java/com/unipass/core/types/UniPassException.kt
@@ -4,6 +4,6 @@ class UserCancelledException(): Exception("User cancelled.")
 
 class UserInterruptedException(): Exception("User interrupted.")
 
-class UserAddressInconsistentException(): Exception("Address inconsistent. User info may expired. Please log in again")
+class UserAddressInconsistentException(): Exception("Address mismatch. Please reconnect to UniPass in the app for authentication.")
 
 class UnKnownException(errorStr: String) : Exception(errorStr)
