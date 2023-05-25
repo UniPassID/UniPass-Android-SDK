f01131329c002d30c1d4666998fbde2fb3888eb7
Ethan <yangyusenhit@gmail.com>
2023-05-25 15:36:06 +0800

    safe: intent-redirectUrl

--- core/src/main/java/com/unipass/core/UniPassActivity.kt
+++ core/src/main/java/com/unipass/core/UniPassActivity.kt
@@ -1,6 +1,7 @@
 package com.unipass.core
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -18,8 +19,19 @@ class UniPassActivity : AppCompatActivity() {
     }
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        intent?.putExtra("resolverId", resolverId)
-        setResult(RESULT_OK, intent);
+
+        val componentName =  intent?.resolveActivity(packageManager)
+        val className = componentName?.className
+        // Make sure that the class name used matches the expected one.
+        if (className.equals("com.unipass.core.UniPassActivity")) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // Remove URI permissions granted in untrusted Intents.
+                intent?.removeFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent?.removeFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+            intent?.putExtra("resolverId", resolverId)
+            setResult(RESULT_OK, intent);
+        }
         finish()
     }
 
